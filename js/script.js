ed3ae19901d15257c5aaf9942b75d8b6356e5ff7
De Patterson <deanna.landry@gmail.com>
2020-02-03 14:33:48 -0800

    working on for loops

--- js/script.js
+++ js/script.js
@@ -1,8 +1,37 @@
 var repository = [
-    { name: 'Charmander', height: 0.6, type: ['fire'], abilities: ['blaze', 'solar-power']},
-    { name: 'Butterfree', height: 1.1, type: ['bug', 'flying'], abilities: ['compoundeyes', 'tinted-lens']},
-    { name: 'Weedle', height: 0.3, type: ['bug', 'poison'], abilities: ['shield-dust', 'run-away']},
-    { name: 'Blastoise', height: 1.6, type: ['water'], abilities: ['rain-dish', 'torrent']},
-    { name: 'Pidgeot' , height: 1.5, type: ['flying', 'normal'], abilities: ['keen-eye', 'tangled-feet', 'big-pecks']}
+    { 
+      name: 'Charmander', 
+      height: 0.6, 
+      type: ['fire'], 
+      abilities: ['blaze', 'solar-power']
+    },
+    { 
+      name: 'Butterfree', 
+      height: 1.1, 
+      type: ['bug', 'flying'], 
+      abilities: ['compoundeyes', 'tinted-lens']
+    },
+    { 
+      name: 'Weedle', 
+      height: 0.3, 
+      type: ['bug', 'poison'], 
+      abilities: ['shield-dust', 'run-away']
+    },
+    { 
+      name: 'Blastoise', 
+      height: 1.6, 
+      type: ['water'], 
+      abilities: ['rain-dish', 'torrent']
+    },
+    { 
+      name: 'Pidgeot' , 
+      height: 1.5, 
+      type: ['flying', 'normal'], 
+      abilities: ['keen-eye', 'tangled-feet', 'big-pecks']
+    }
 ];
 
+var pokeheight = repository.height;
+for (let i = 0; i < repository.length; i++) {
+    let pokemon = repository[i];
+}
